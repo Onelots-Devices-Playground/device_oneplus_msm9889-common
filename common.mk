89293fbbc547ad9d3eebdf941ded5d268ad87c44
Onelots <onelots@onelots.fr>
2025-01-24 23:45:14 +0100

    msm8998: gapps: reduce to mini to fit
    Signed-off-by: Onelots <onelots@onelots.fr>

--- common.mk
+++ common.mk
@@ -11,6 +11,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/non_ab_device.mk)
 
 $(call inherit-product, vendor/oneplus/msm8998-common/msm8998-common-vendor.mk)
 
+# Use mini_gapps instead of full gapps
+TARGET_USES_MINI_GAPPS := true
+
 # Setup dalvik vm configs
 $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
 
